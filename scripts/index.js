880a0271b51b365bc18820b77c22851f2135821b
TSilvaForte <tamara.sfok@gmail.com>
2024-06-11 23:13:29 -0300

    Final version with testing

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,3 @@
-// La actividad es el concepto del mundo real que estamos representando en esta clase o molde
-class Activity {
-    constructor(id, title, description, imgUrl) {
-        this.id = id
-        this.title = title
-        this.description = description
-        this.imgUrl = imgUrl
-    }
-}
-
-// Las respuestas que obtengamos, las vamos a guardar en un repositorio, en este caso en un array
-class Repository {
-    constructor() {
-        this.activities = []
-        this.id = 1
-    }
-
-    // Método que retorna todas las actividades dentro del array
-    getAllActivities() {
-        return this.activities
-    }
-
-    // Método que crea y guarda una nueva actividad
-    createActivity(title, description, imgUrl) {
-        const activity = new Activity(this.id++, title, description, imgUrl);
-        this.activities.push(activity)
-    }
-
-    // Método que elimina una actividad por id
-    deleteActivity(id) {
-        this.activities = this.activities.filter((act) => act.id !== id)
-    }
-}
-
 const repository = new Repository()
 
 // Ahora vamos a seleccionar los elementos con los que queremos trabajar
--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -1,5 +1,31 @@
+const {Activity, Repository} = require ("../scripts/models.js")
+
 describe("demo", function () {
   it("Este test debe pasar siempre", function () {
     expect(4 + 2).toBe(6);
   });
 });
+
+describe("Testing Activity", () => {
+  it ("las instancias de Activity tienen los atributos requeridos", () => {
+      const newActivity= new Activity ()
+      const attributes = Object.keys (newActivity)
+      expect (attributes.length).toBe(4)
+      expect (attributes).toContain("id")
+      expect (attributes).toContain("title")
+      expect (attributes).toContain("description")
+      expect (attributes).toContain("imgUrl")
+    })
+})
+
+describe ("Testing Repository", () => {
+  it ("las instancias de Repository tienen los atributos requeridos", () => {
+    const newRepository = new Repository ()
+    const attributes = Object.keys (newRepository)
+      expect (attributes.length).toBe(2)
+      expect (attributes).toContain("activities")
+      expect (attributes).toContain("id")
+      expect (newRepository.activities).toEqual([])
+      expect (newRepository.id).toBe(1)
+  })
+})
